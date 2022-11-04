de8b20a4d0b3d291a076b31e74c31a3e40262e88
Shekhar9398 <117426760+Shekhar9398@users.noreply.github.com>
2022-11-04 10:54:02 +0530

    Add files via upload

---
+++ DemoTableView/DemoTableView/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  DemoTableView
+//
+//  Created by Shraddha Manwar on 28/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var contactTableview: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
